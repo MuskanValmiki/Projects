63711078613acedd30d7d3ca589a54c9fc639870
Muskan Valmikee <83203071+MuskanValmiki@users.noreply.github.com>
2021-10-19 12:24:59 +0530

    Add files via upload

---
+++ project4.js
@@ -0,0 +1,8 @@
+//link create
+var name=prompt("enter any name")
+alllinks=document.links;
+Array.from(alllinks).forEach((eachLink)=>{
+    if(eachLink.text==name){
+        console.log(eachLink);
+    }
+})
